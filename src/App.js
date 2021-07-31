456195f190b68906a0999e32f25d6ea76bde1652
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-07-31 22:18:52 +0300

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
+
+import Header from "./components/Header";
+import TransactionTable from "./components/TransactionTable";
+import Chart from "./components/Chart";
+import AddCategory from "./components/AddCategory";
+import AddTransaction from "./components/AddTransaction";
+
+export default function App() {
+    const [categories, setCategories] = useState([]);
+    const [transactions, setTransactions] = useState([]);
+    const [showAddCategory, setShowAddCategory] = useState(true);
+    const [showAddTransaction, setShowAddTransaction] = useState(false);
+    const [activeCategory, setActiveCategory] = useState("");
+
+    const removeTransaction = (index) => {
+        const newTransactions = transactions.filter((transaction, idx) => {
+            return idx !== index;
+        });
+        setTransactions(newTransactions);
+    };
+
+    const filterTransactions = () => {
+        return transactions
+            .filter((transaction) =>
+                activeCategory ? transaction.category.name === activeCategory : true
+            )
+            .sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1));
+    };
+
+    if (showAddCategory) {
+        return (
+            <AddCategory
+                setShowAddCategory={setShowAddCategory}
+                setCategories={setCategories}
+            />
+        );
+    }
+    if (showAddTransaction) {
+        return (
+            <AddTransaction
+                categories={categories}
+                setTransactions={setTransactions}
+                setShowAddTransaction={setShowAddTransaction}
+            />
+        );
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <Header
+                    categories={categories}
+                    activeCategory={activeCategory}
+                    setShowAddCategory={setShowAddCategory}
+                    setActiveCategory={setActiveCategory}
+                />
+            </div>
+            <div className="row">
+                <div className="col">
+                    <TransactionTable
+                        setShowAddTransaction={setShowAddTransaction}
+                        removeTransaction={removeTransaction}
+                        transactions={filterTransactions(transactions)}
+                    />
+                </div>
+                <div className="col">
+                    <Chart transactions={filterTransactions(transactions)} />
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
